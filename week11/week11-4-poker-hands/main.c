1afefc0846b4674b2f95d0853d60937cb179e5db
Ted Song <kitae040522@gmail.com>
2023-12-12 23:37:29 +0900

    FEAT: 11-4 과제 풀이 수정

--- week11/week11-4-poker-hands/main.c
+++ week11/week11-4-poker-hands/main.c
@@ -9,6 +9,8 @@ typedef struct
 } CARD;
 
 CARD deck[7];
+
+int suit_count[4];
 int straight, flush, four, three, pair;
 
 int cmp(const void *_n, const void *_m)
@@ -22,34 +24,26 @@ int cmp(const void *_n, const void *_m)
 
 int main()
 {
-    for (int i = 0; i < 7; ++i) scanf("%c%d,", &deck[i].suit, &deck[i].rank);
-    qsort(deck, 7, sizeof(CARD), cmp);
-    //for (int i = 0; i < 7; ++i) printf("suit: %c, rank: %d\n", deck[i].suit, deck[i].rank);
-
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < 7; ++i)
     {
-        int straight_flag = 1, flush_flag = 1;
-        for (int j = 0; j < 4; ++j)
-        {
-            if (deck[i + j].rank + 1 != deck[i + j + 1].rank)
-            {
-                straight_flag = 0;
-                break;
-            }
-        }
-        for (int j = 0; j < 4; ++j)
-        {
-            if (deck[i + j].suit != deck[i + j + 1].suit)
-            {
-                flush_flag = 0;
-                break;
-            }
-        }
-        straight = straight_flag;
-        flush = flush_flag;
-    }
-    for (int i = 0; i < 4; ++i) if (deck[i].rank == deck[i + 1].rank && deck[i + 1].rank == deck[i + 2].rank && deck[i + 2].rank == deck[i + 3].rank) four = 1;
-    for (int i = 0; i < 5; ++i) if (deck[i].rank == deck[i + 1].rank && deck[i + 1].rank == deck[i + 2].rank) three = 1;
+        scanf("%c%d,", &deck[i].suit, &deck[i].rank);
+        if (deck[i].suit == 'h') suit_count[0]++;
+        else if (deck[i].suit == 'c') suit_count[1]++;
+        else if (deck[i].suit == 'd') suit_count[2]++;
+        else if (deck[i].suit == 's') suit_count[3]++;
+    }   
+    for (int i = 0; i < 4; ++i) if (suit_count[i] >= 5) {flush = 1; break;}
+
+    qsort(deck, 7, sizeof(CARD), cmp);
+    for (int i = 0; i < 3; ++i) if (deck[i].rank + 1 == deck[i + 1].rank &&
+                                    deck[i + 1].rank + 1 == deck[i + 2].rank &&
+                                    deck[i + 2].rank + 1 == deck[i + 3].rank &&
+                                    deck[i + 3].rank + 1 == deck[i + 4].rank) straight = 1;
+    for (int i = 0; i < 4; ++i) if (deck[i].rank == deck[i + 1].rank &&
+                                    deck[i + 1].rank == deck[i + 2].rank &&
+                                    deck[i + 2].rank == deck[i + 3].rank) four = 1;
+    for (int i = 0; i < 5; ++i) if (deck[i].rank == deck[i + 1].rank &&
+                                    deck[i + 1].rank == deck[i + 2].rank) three = 1;
     for (int i = 0; i < 6; ++i) if (deck[i].rank == deck[i + 1].rank) pair++;
 
     if (straight && flush) printf("straight flush");
