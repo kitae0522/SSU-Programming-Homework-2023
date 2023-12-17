a1026e2b24c8b19961e5944fba8af88c6f81b8cf
Ted Song <kitae040522@gmail.com>
2023-12-18 02:33:03 +0900

    FEAT: 3-1 과제 풀이 수정

--- week03/week3-1-pattern-counter/main.c
+++ week03/week3-1-pattern-counter/main.c
@@ -1,25 +1,31 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+char *string_slice(const char *string, int start, int end)
+{
+    char *new_string = (char *) malloc(sizeof(char) * (end - start + 1));
+    memset(new_string, 0, sizeof(char));
+    for (int i = start; i < end; ++i) new_string[i - start] = string[i];
+    new_string[end - start + 1] = '\0';
+    return new_string;
+}
 
 int main()
 {
-    char s1[1001], s2[1001];
-    scanf("%[^\n]s", s1);
-    scanf("%s", s2);
-    int count = 0;
-    for (int i = 0; i <= strlen(s1) - strlen(s2); ++i)
+    char reference_string[1001]; scanf("%[^\n]s", reference_string); getchar();
+    char target_string[1001]; scanf("%[^\n]s", target_string); getchar();
+
+    int size = 0, result = 0;
+    while (reference_string[size] != '\0' || size < strlen(reference_string))
     {
-        int match = 1;
-        for (int j = 0; j < strlen(s2); ++j)
-        {
-            if (s1[i + j] != s2[j])
-            {
-                match = 0;
-                break;
-            }
-        }
-        if (match) ++count;
+        char *slice_string = string_slice(reference_string, size, strlen(target_string) + size);
+        int str_res = strcmp(slice_string, target_string);
+        if (!str_res) result++, size += strlen(target_string);
+        else size++;
     }
-    printf("%d\n", count);
+    printf("%d", result);
+
     return 0;
 }
