d846302d13fa7df8a56950606f544ecedaacc79e
Ted Song <kitae040522@gmail.com>
2023-12-18 02:33:17 +0900

    FEAT: 9-4 과제 풀이 수정

--- week09/week9-4-snail-array/main.c
+++ week09/week9-4-snail-array/main.c
@@ -1,32 +1,29 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-//1 2 3 4 5
-//16 17 18 19 6
-//15 24 25 20 7
-//14 23 22 21 8
-//13 12 11 10 9
-// 5, 4, 4, 3, 3, 2, 2, 1, 1
-
-int N, R, C, X = 1, D = 1;
+int n, r, c, x = 1, d = 1;
 
 int main()
 {
-    scanf("%d", &N);
-    int **A = (int **) malloc(sizeof(int *) * N);
-    for (int i = 0; i < N; ++i) A[i] = (int *) malloc(sizeof(int) * N);
+    scanf("%d", &n);
+    int** arr = (int**) malloc(sizeof(int*) * n);
+    for (int i = 0; i < n; ++i) arr[i] = (int*) malloc(sizeof(int) * n);
 
-    for (int i = 0; i < N; ++i) A[R][C] = X++, C += D;
-    C--;
-    for (int i = N - 1; i > 0; --i)
+    for (int i = 0; i < n; ++i) arr[r][c] = x++, c += d;
+    c--;
+
+    for (int i = n - 1; i > 0; --i)
     {
-        for (int j = 0; j < i; ++j) R += D, A[R][C] = X++;
-        D *= -1;
-        for (int j = 0; j < i; ++j) C += D, A[R][C] = X++;
+        for (int j = 0; j < i; ++j) r += d, arr[r][c] = x++;
+        for (int j = 0; j < i; ++j) c -= d, arr[r][c] = x++;
+        d *= -1;
     }
-    for (int i = 0; i < N; ++i)
+
+    for (int i = 0; i < n; ++i)
     {
-        for (int j = 0; j < N; ++j) printf("%d ", A[i][j]);
+        for (int j = 0; j < n; ++j) printf("%d ", arr[i][j]);
         printf("\n");
     }
     return 0;
